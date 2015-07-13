993d01273a4d0269eed350e79923acef3e398a46
Zachary McNellis <zmcnell@g.clemson.edu>
2015-07-13 13:55:32 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+var Slack = require('slack-client');
+
+var token = 'xoxb-7212819110-AT2jJNWQlmR1WYLX2FBHdtAH';
+
+var slack = new Slack(token, true, true);
+
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+        .map(function (k) { return slack.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+
+    var groups = Object.keys(slack.groups)
+        .map(function (k) { return slack.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+});
+
+slack.on('message', function(message) {
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var user = slack.getUserByID(message.user);
+
+    if (message.type === 'message') {
+        console.log(channel.name + ':' + user.name + ':' + message.text);
+    }
+});
+
+slack.login();
