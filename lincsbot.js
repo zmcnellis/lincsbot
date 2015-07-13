084fc5a4c32ea05d6e493712d1e9c61cd7af86d1
Zachary McNellis <zmcnell@g.clemson.edu>
2015-07-13 13:34:06 -0400

    working

--- lincsbot.js
+++ lincsbot.js
@@ -4,25 +4,6 @@ var token = 'xoxb-7212819110-AT2jJNWQlmR1WYLX2FBHdtAH';
 
 var slack = new Slack(token, true, true);
 
-var makeMention = function(userId) {
-    return '<@' + userId + '>';
-};
-
-var isDirect = function(userId, messageText) {
-    var userTag = makeMention(userId);
-    return messageText &&
-           messageText.length >= userTag.length &&
-           messageText.substr(0, userTag.length) === userTag;
-};
-
-var getOnlineHumansForChannel = function(channel) {
-    if (!channel) return [];
-
-    return (channel.members || [])
-        .map(function(id) { return slack.users[id]; })
-        .filter(function(u) { return !!u && !u.is_bot && u.presence === 'active'; });
-};
-
 slack.on('open', function () {
     var channels = Object.keys(slack.channels)
         .map(function (k) { return slack.channels[k]; })
@@ -52,14 +33,8 @@ slack.on('message', function(message) {
     var channel = slack.getChannelGroupOrDMByID(message.channel);
     var user = slack.getUserByID(message.user);
 
-    if (message.type === 'message' && isDirect(slack.self.id, message.text)) {
-        var trimmedMessage = message.text.substr(makeMention(slack.self.id).length).trim();
-        
-        var onlineUsers = getOnlineHumansForChannel(channel)
-            .filter(function(u) { return u.id != user.id; })
-            .map(function(u) { return makeMention(u.id); });
-        
-        channel.send(onlineUsers.join(', ') + '\r\n' + user.real_name + 'said: ' + trimmedMessage);
+    if (message.type === 'message') {
+        console.log(channel.name + ':' + user.name + ':' + message.text);
     }
 });
 
