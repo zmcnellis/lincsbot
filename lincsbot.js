ee3f336507aa2bd2ee7370196f0db6afc05ec62f
Zachary McNellis <zmcnell@g.clemson.edu>
2015-07-13 13:16:26 -0400

    the new lincsbot

--- lincsbot.js
+++ lincsbot.js
@@ -1,16 +1,66 @@
-module.exports = function (req, res, next) {
-  var userName = req.body.user_name;
-  var botPayload = {
-    text : 'Hello, @' + userName + '!',
-    user : 'U0768Q338',
-    type : 'message',
-    as_user : true
-  };
- 
-  // avoid infinite loop
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
-}
+var Slack = require('slack-client');
+
+var token = 'xoxb-7212819110-AT2jJNWQlmR1WYLX2FBHdtAH';
+
+var slack = new Slack(token, true, true);
+
+var makeMention = function(userId) {
+    return '<@' + userId + '>';
+};
+
+var isDirect = function(userId, messageText) {
+    var userTag = makeMention(userId);
+    return messageText &&
+           messageText.length >= userTag.length &&
+           messageText.substr(0, userTag.length) === userTag;
+};
+
+var getOnlineHumansForChannel = function(channel) {
+    if (!channel) return [];
+
+    return (channel.members || [])
+        .map(function(id) { return slack.users[id]; }
+        .filter(function(u) { return !!u && !u.is_bot && u.presence === 'active'; });
+};
+
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+        .map(function (k) { return slack.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+
+    var groups = Object.keys(slack.groups)
+        .map(function (k) { return slack.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+});
+
+slack.on('message', function(message) {
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var user = slack.getUserByID(message.user);
+
+    if (message.type === 'message' && isDirect(slack.self.id, message.text)) {
+        var trimmedMessage = message.text.substr(makeMention(slack.self.id).length).trim();
+        
+        var onlineUsers = getOnlineHumansForChannel(channel)
+            .filter(function(u) { return u.id != user.id; })
+            .map(function(u) { return makeMention(u.id); });
+        
+        channel.send(onlineUsers.join(', ') + '\r\n' + user.real_name + 'said: ' + trimmedMessage);
+    }
+});
+
+slack.login();
