861243bb10e9ffb724839ff82a2bc40b4cbe3907
Zachary McNellis <zmcnell@g.clemson.edu>
2015-07-13 11:20:31 -0400

    username

--- lincsbot.js
+++ lincsbot.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello, @' + userName + '!',
-    username : 'lincsbot'
+    user : 'lincsbot'
   };
  
   // avoid infinite loop
